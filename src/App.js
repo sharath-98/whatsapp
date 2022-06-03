8cfce54b83fd073c0ef8459ee8f19e694b94389c
sharath-98 <sharathchander.p@gmail.com>
2022-06-03 14:29:25 -0500

    Login page design

--- src/App.js
+++ src/App.js
@@ -1,30 +1,40 @@
-import { Fragment } from 'react';
+import { Fragment, useState } from 'react';
 import { Route, BrowserRouter as Router, Routes } from 'react-router-dom';
 import './App.css';
 import Chat from './Chat';
 import Leftpane from './Leftpane';
+import Login from './Login';
 
 function App() {
+  const [user, setUser] = useState(null);
   return (
     <Router>
       <div className="app">
-          <div className='appBody'>
-        <Leftpane/>
-      <Routes>
-        <Route path='/users/:userId' element={
-            <Fragment>
-              <Chat/>
-            </Fragment>
-        }>
-        </Route>
-        <Route path='/' element={
-          <Fragment>
-            <Chat/>
-          </Fragment>
-        }>
-        </Route>
-      </Routes>
-        </div>
+        {
+          !user? (
+            <Login/>
+          ):
+          (
+            <div className='appBody'>
+              <Leftpane/>
+              <Routes>
+                <Route path='/users/:userId' element={
+                    <Fragment>
+                      <Chat/>
+                    </Fragment>
+                }>
+                </Route>
+                <Route path='/' element={
+                  <Fragment>
+                    <Chat/>
+                  </Fragment>
+                }>
+                </Route>
+              </Routes>
+            </div>
+          )
+        }
+        
     </div>
     </Router>
   );
---
+++ src/Login.js
@@ -0,0 +1,25 @@
+import { Button } from '@material-ui/core'
+import React from 'react'
+import './Login.css'
+
+function Login() {
+
+    const signIn = (e) =>
+    {
+
+    };
+    
+  return (
+    <div className='login'>
+       <div className="home">
+               <img src="https://upload.wikimedia.org/wikipedia/commons/6/6b/WhatsApp.svg" alt=""/> 
+                <div className="login_text">
+                    <h1>Sign in to Whatsapp</h1>
+                </div>
+                <Button type="submit" onClick={signIn}>Sign in With Google</Button>
+           </div>
+    </div>
+  )
+}
+
+export default Login
