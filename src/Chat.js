2979bcfdc95cf858bb436833bd14a916d4259f4d
sharath-98 <sharathchander.p@gmail.com>
2022-06-03 17:44:20 -0500

    removed text decorations and corrected page reloading after sending a message

--- src/Chat.js
+++ src/Chat.js
@@ -1,5 +1,5 @@
-import { Avatar, IconButton, Snackbar } from '@material-ui/core'
-import { AttachFile, InsertEmoticon, Mic, MoreVert, SearchOutlined } from '@material-ui/icons';
+import { Avatar, IconButton } from '@material-ui/core'
+import { AttachFile, EmojiEmotions, InsertEmoticon, Mic, MoreVert, SearchOutlined } from '@material-ui/icons';
 import React, { useEffect, useState } from 'react'
 import { useParams } from 'react-router-dom';
 import './Chat.css'
@@ -48,7 +48,10 @@ function Chat() {
             <Avatar src={`https://avatars.dicebear.com/api/human/${seed}.svg`}/>
             <div className='user_info'>
                 <h3>{userName}</h3>
-                <p>{new Date(messages[messages.length-1]?.timestamp.toDate()).toUTCString()}</p>
+                <p className='chat-room-last-seen'>
+                        Last seen {" "}
+                        {new Date(messages[messages.length - 1]?.timestamp?.toDate()).toUTCString()}
+                    </p>
             </div>
             <div className='chat_header_right'>
                 <IconButton><SearchOutlined/></IconButton>
@@ -70,8 +73,8 @@ function Chat() {
             }
         </div>
         <div className='chat_footer'>
-                <InsertEmoticon />
-                <form onSubmit={sendMessage}>
+                <EmojiEmotions/>
+            <form onSubmit={sendMessage}>
                     <input value={input} onChange={(e) => setInput(e.target.value)} type="text" placeholder="Type a message"/>
                     <button type="submit"> Send a Message</button>
                 </form>
--- src/Chatlist.js
+++ src/Chatlist.js
@@ -3,31 +3,51 @@ import React, { useEffect, useState } from 'react'
 import { Link } from 'react-router-dom';
 import './Chatlist.css'
 import db from './firebase';
+import firebase from "firebase"
 
 function Chatlist({newChat, id, name}) {
     const [seed, setSeed] = useState('');
+    const [messages, setMessages] = useState([]);
+    /*
+       useEffect(() => {
+            if(id){
+                db.collection('rooms').doc(id).collection('messages').get()
+                .orderBy('timestamp','desc')
+                .onSnapshot((snapshot) => {
+                setMessages(snapshot.docs.map((doc) => doc.data()))
+                })
+            }
+        }, [id]);
+        */
     
     useEffect(()=>{
         setSeed(Math.floor(Math.random()*50000));
     }, []);
 
-    const createChat = () =>{
+    const createChat = async () =>{
         const newChatName = prompt("Please enter the name of the chat");
-        if(newChatName)
+        if(newChatName!="")
         {
-            db.collection("rooms").add({
+            const doc_ref = await db.collection("rooms").add({
                 name:newChatName
             });
+
+            db.collection("rooms").doc(doc_ref.id).collection("messages")
+            .add({
+                message:"This is an auto-generated message, a new Chat Room is created. Happy chatting!",
+                name:"System",
+                timestamp:firebase.firestore.FieldValue.serverTimestamp(),
+                })
         }
     };
   
     return !newChat ? (
-    <Link to={`/users/${id}`}>
+    <Link to={`/users/${id}`} key={id}>
         <div className='chatlist'>
             <Avatar src={`https://avatars.dicebear.com/api/human/${seed}.svg`}/>
             <div className='chatInfo'>
                 <h2>{name}</h2>
-                <p>Last message ...</p>
+                <p>{messages[0]?.message}</p>
             </div>
         </div>
     </Link>
